870671ee4b02150e8f8620df951c7648aa2f08ca
Humberto Zayas <soundsinhd@gmail.com>
2021-07-29 20:21:13 -0400

    created demo data

---
+++ mixins/items.js
@@ -0,0 +1,38 @@
+const kits = [
+  {
+    id: 1,
+    name: 'Kit 1',
+    price: 12.00,
+    category: 'Hip Hop',
+    description: 'Kit 1 hip hop drums'
+  },
+  {
+    id: 2,
+    name: 'Kit 2',
+    price: 10.00,
+    category: 'Trap',
+    description: 'Kit 2 trap drums'
+  },
+  {
+    id: 3,
+    name: 'Kit 3',
+    price: 2.00,
+    category: 'Percussion',
+    description: 'Kit 3 percussion drums'
+  },
+  {
+    id: 4,
+    name: 'Kit 4',
+    price: 12.00,
+    category: 'Break Beat',
+    description: 'Kit 4 break beats'
+  }
+]
+
+export default {
+  data () {
+    return {
+      kits
+    }
+  }
+}
